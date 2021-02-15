cd87bd85db20e64e918079e8663874a37b792bc0
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-15 11:09:38 -0600

    added sedan class to extend vehicle and gave sedan class 2 properities: size and mpg

--- inheritance.js
+++ inheritance.js
@@ -4,7 +4,7 @@ class Vehicle {
         this.numOfWheels = numOfWheels;
         this.type = type;
     }
-    aboutVehicle() {
+    showVehicleInfo() {
         return console.log(`Vehicle Type: ${this.type}\nManufacturer: ${this.manufacturer}\nNumber of Wheels: ${this.numOfWheels}`);
     };
 };
@@ -22,29 +22,51 @@ class Truck extends Vehicle {
 };
 
 
+class Sedan extends Vehicle {
+    constructor(hasTruckBed, size, mpg) {
+        super(hasTruckBed);
+        this.size = size;
+        this.mpg = mpg;
+    }
+    showSedanVehicleInfo(){
+        return console.log(`Vehicle Size: ${this.size}\nMiles per Gallon: ${this.mpg} mpg`);
+    };
+};
+
+
 let vehicleOne = new Vehicle("Ford", 4, "car");
 let vehicleOneTruck = new Truck(4, 4.5, false);
-vehicleOne.aboutVehicle();
+let vehicleOneSedan = new Sedan(false, "medium", 27);
+vehicleOne.showVehicleInfo();
 vehicleOneTruck.showTruckVehicleInfo();
+vehicleOneSedan.showSedanVehicleInfo();
 
 let vehicleTwo = new Vehicle("Audi", 4, "sports car");
 let vehicleTwoTruck = new Truck(4, 2, false);
-vehicleTwo.aboutVehicle();
+let vehicleTwoSedan = new Sedan(false, "small", 38);
+vehicleTwo.showVehicleInfo();
 vehicleTwoTruck.showTruckVehicleInfo();
+vehicleTwoSedan.showSedanVehicleInfo();
 
 let vehicleThree = new Vehicle("Harley-Davidson", 2, "motorcycle");
 let vehicleThreeTruck = new Truck(2, 0, false);
-vehicleThree.aboutVehicle();
+let vehicleThreeSedan = new Sedan(false, "small", 50);
+vehicleThree.showVehicleInfo();
 vehicleThreeTruck.showTruckVehicleInfo();
+vehicleThreeSedan.showSedanVehicleInfo();
 
 let vehicleFour = new Vehicle("Dodge", 6, "truck");
 let vehicleFourTruck = new Truck(6, 6, true);
-vehicleFour.aboutVehicle();
+let vehicleFourSedan = new Sedan(true, "medium", 25);
+vehicleFour.showVehicleInfo();
 vehicleFourTruck.showTruckVehicleInfo();
+vehicleFourSedan.showSedanVehicleInfo();
 
 let vehicleFive = new Vehicle("Polaris", 3, "roadster");
 let vehicleFiveTruck = new Truck(3, 0, false);
-vehicleFive.aboutVehicle();
+let vehicleFiveSedan = new Sedan(false, "small", 45);
+vehicleFive.showVehicleInfo();
 vehicleFiveTruck.showTruckVehicleInfo();
+vehicleFiveSedan.showSedanVehicleInfo();
 
 
