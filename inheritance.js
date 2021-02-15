a27b0a2e256529043f009a712d20c0e323f70671
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-15 10:15:37 -0600

    added truck inheritance to vehicle class

--- inheritance.js
+++ inheritance.js
@@ -3,14 +3,29 @@ class Vehicle {
         this.manufacturer = manufacturer;
         this.numOfWheels = numOfWheels;
     }
+    aboutVehicle() {
+        return console.log(`Vehicle Type: ${this.type}\nManufacturer: ${this.manufacturer}\nNumber of Wheels: ${this.numOfWheels}`);
+    };
 };
 
-Vehicle.prototype.aboutVehicle = function() {
-    console.log(`Vehicle Type: ${this.type}\nManufacturer: ${this.manufacturer}\nNumber of Wheels: ${this.numOfWheels}`);
+
+class Truck extends Vehicle {
+    constructor(numOfWheels, numOfDoors, hasTruckBed) {
+        super(numOfWheels);
+        this.numOfDoors = numOfDoors;
+        this.hasTruckBed = hasTruckBed;
+    }
+    showTruckVehicleInfo() {
+        return console.log(`No. of Doors: ${this.numOfDoors}\n${this.type} has Truck Bed: ${this.hasTruckBed}`);
+    };
 };
 
-let vehicleOne = new Vehicle("Ford", 4);
+
+let vehicleOne = new Vehicle(4, "Ford", 4);
+let vehicleOneTruck = new Truck(4, false);
+vehicleOneTruck.showTruckVehicleInfo();
 vehicleOne.type = "Car";
+
 let vehicleTwo = new Vehicle("Audi", 4);
 vehicleTwo.type = "Sports Car";
 let vehicleThree = new Vehicle("Harley-Davidson", 2);
@@ -22,3 +37,5 @@ vehicleOne.aboutVehicle();
 vehicleTwo.aboutVehicle();
 vehicleThree.aboutVehicle();
 vehicleFour.aboutVehicle();
+
+
