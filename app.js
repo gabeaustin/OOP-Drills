45dcdb94a3d89ec89a28fbee79c3e22baa187e53
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-14 23:25:10 -0600

    fixed code to have the sayHello function to log for all 5 people

--- app.js
+++ app.js
@@ -5,21 +5,42 @@
 // Then give each one a second property, sayHello, which is a function that will console log the string “Hello! My name is ________”.
 
 let person1 = {
-    name: "JW Fowler"
+    name: "JW Fowler",
+    sayHello: function() {
+        console.log(`Hello! My name is ${this.name}.`)
+    }
 };
 
 let person2 = {
-    name: "JC Reynolds"
+    name: "JC Reynolds",
+    sayHello: function() {
+        console.log(`Hello! My name is ${this.name}.`)
+    }
 };
 
 let person3 = {
-    name: "BK Scott"
+    name: "BK Scott",
+    sayHello: function() {
+        console.log(`Hello! My name is ${this.name}.`)
+    }
 };
 
 let person4 = {
-    name: "MIT Austin"
+    name: "MIT Austin",
+    sayHello: function() {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
 };
 
 let person5 = {
-   name:  "ZBA Jones"
+   name:  "ZBA Jones",
+   sayHello: function() {
+       console.log(`Hello! My name is ${this.name}.`)
+   }
 };
+
+person1.sayHello();
+person2.sayHello();
+person3.sayHello();
+person4.sayHello();
+person5.sayHello();
