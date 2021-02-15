0f095c85c61e0739df494ebb983b1c7e2a3576a9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-14 23:13:19 -0600

    added 5 object literals and included a name property

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert("i work");
+// Object Literals, Pseudo Classes and Methods
+
+// x - Write 5 object literals, have them named person1-person5. 
+// x - Give each a name property and have this be a name of a friend or family member. 
+// Then give each one a second property, sayHello, which is a function that will console log the string “Hello! My name is ________”.
+
+let person1 = {
+    name: "JW Fowler"
+};
+
+let person2 = {
+    name: "JC Reynolds"
+};
+
+let person3 = {
+    name: "BK Scott"
+};
+
+let person4 = {
+    name: "MIT Austin"
+};
+
+let person5 = {
+   name:  "ZBA Jones"
+};
