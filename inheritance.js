7b16f7ddb9e28ef11f590bc3d3e5d8ce74d203d6
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-15 15:57:39 -0600

    continued to work on override function

--- inheritance.js
+++ inheritance.js
@@ -3,13 +3,12 @@ class Vehicle {
         this.manufacturer = manufacturer;
         this.numOfWheels = numOfWheels;
         this.type = type;
-    }
+    };
 
     showVehicleInfo() {
         return console.log(`Vehicle Type: ${this.type}\nManufacturer: ${this.manufacturer}\nNumber of Wheels: ${this.numOfWheels}`);
-    };
+    };   
 };
-
 
 
 class Truck extends Vehicle {
@@ -17,17 +16,7 @@ class Truck extends Vehicle {
         super(numOfWheels);
         this.numOfDoors = numOfDoors;
         this.hasTruckBed = hasTruckBed;
-    }
-    
-    showTruckVehicleInfo() {
-        return console.log(`No. of Doors: ${this.numOfDoors}\nHas Truck Bed: ${this.hasTruckBed}`);
     };
-    
-    // Method Overriding
-    showVehicleInfo() {
-        console.log(`This vehicle has ${this.numOfDoors} number of doors and has a truck bed - ${this.hasTruckBed}.`);
-        // can I put an if stmt here to check for "true" and change to "has/doesn't have"??
-    }
 };
 
 
@@ -37,10 +26,6 @@ class Sedan extends Vehicle {
         this.size = size;
         this.mpg = mpg;
     }
-
-    showSedanVehicleInfo() {
-        return console.log(`Vehicle Size: ${this.size}\nMiles per Gallon: ${this.mpg} mpg`);
-    };
 };
 
 
@@ -49,60 +34,73 @@ class Motorcycle extends Vehicle {
         super(numOfDoors);
         this.handleBarsSteeringWheel = handleBarsSteeringWheel;
     }
-
-    showMotorcycleVehicleInfo() {
-        return console.log(`Handlebars / Steering Wheel: ${this.handleBarsSteeringWheel}\n`)
-    };
 };
 
-
-
+// Method Override
+Vehicle.prototype.showVehicleInfo = function() {
+    console.log(`The ${this.manufacturer} ${this.type} has ${this.numOfWheels} wheels and has a ${this.handleBarsSteeringWheel}.`);
+    // console.log(`This vehicle has ${this.numOfDoors} number of doors and has a truck bed - ${this.hasTruckBed}.`);   
+};
 
 
 let vehicleOne = new Vehicle("Ford", 4, "car");
 let vehicleOneTruck = new Truck(4, 4.5, false);
 let vehicleOneSedan = new Sedan(false, "medium", 27);
 let vehicleOneMotorcyle = new Motorcycle("steering wheel", 4);
-vehicleOne.showVehicleInfo();
+// vehicleOne.showVehicleInfo();
 vehicleOneTruck.showVehicleInfo();
+vehicleOneSedan.showVehicleInfo();
+vehicleOneMotorcyle.showVehicleInfo();
 // vehicleOneTruck.showTruckVehicleInfo();
-vehicleOneSedan.showSedanVehicleInfo();
-vehicleOneMotorcyle.showMotorcycleVehicleInfo();
+// vehicleOneSedan.showSedanVehicleInfo();
+// vehicleOneMotorcyle.showMotorcycleVehicleInfo();
 
 let vehicleTwo = new Vehicle("Audi", 4, "sports car");
 let vehicleTwoTruck = new Truck(4, 2, false);
 let vehicleTwoSedan = new Sedan(false, "small", 38);
 let vehicleTwoMotorcycle = new Motorcycle("steering wheel", 2);
-vehicleTwo.showVehicleInfo();
-vehicleTwoTruck.showTruckVehicleInfo();
-vehicleTwoSedan.showSedanVehicleInfo();
-vehicleTwoMotorcycle.showMotorcycleVehicleInfo();
+// vehicleTwo.showVehicleInfo();
+// vehicleTwoTruck.showTruckVehicleInfo();
+vehicleTwoTruck.showVehicleInfo();
+vehicleTwoSedan.showVehicleInfo();
+vehicleTwoMotorcycle.showVehicleInfo();
+// vehicleTwoSedan.showSedanVehicleInfo();
+// vehicleTwoMotorcycle.showMotorcycleVehicleInfo();
 
 let vehicleThree = new Vehicle("Harley-Davidson", 2, "motorcycle");
 let vehicleThreeTruck = new Truck(2, 0, false);
 let vehicleThreeSedan = new Sedan(false, "small", 50);
 let vehicleThreeMotorcycle = new Motorcycle("handle bars", 0);
-vehicleThree.showVehicleInfo();
-vehicleThreeTruck.showTruckVehicleInfo();
-vehicleThreeSedan.showSedanVehicleInfo();
-vehicleThreeMotorcycle.showMotorcycleVehicleInfo();
+vehicleThreeTruck.showVehicleInfo();
+vehicleThreeSedan.showVehicleInfo();
+vehicleThreeMotorcycle.showVehicleInfo();
+// vehicleThree.showVehicleInfo();
+// vehicleThreeTruck.showTruckVehicleInfo();
+// vehicleThreeSedan.showSedanVehicleInfo();
+// vehicleThreeMotorcycle.showMotorcycleVehicleInfo();
 
 let vehicleFour = new Vehicle("Dodge", 6, "truck");
 let vehicleFourTruck = new Truck(6, 6, true);
 let vehicleFourSedan = new Sedan(true, "medium", 25);
 let vehicleFourMotorcycle = new Motorcycle("steering wheel", 6);
-vehicleFour.showVehicleInfo();
-vehicleFourTruck.showTruckVehicleInfo();
-vehicleFourSedan.showSedanVehicleInfo();
-vehicleFourMotorcycle.showMotorcycleVehicleInfo();
+vehicleFourTruck.showVehicleInfo();
+vehicleFourSedan.showVehicleInfo();
+vehicleFourMotorcycle.showVehicleInfo();
+// vehicleFour.showVehicleInfo();
+// vehicleFourTruck.showTruckVehicleInfo();
+// vehicleFourSedan.showSedanVehicleInfo();
+// vehicleFourMotorcycle.showMotorcycleVehicleInfo();
 
 let vehicleFive = new Vehicle("Polaris", 3, "roadster");
 let vehicleFiveTruck = new Truck(3, 0, false);
 let vehicleFiveSedan = new Sedan(false, "small", 45);
 let vehicleFiveMotorcyle = new Motorcycle("steering wheel", 0);
-vehicleFive.showVehicleInfo();
-vehicleFiveTruck.showTruckVehicleInfo();
-vehicleFiveSedan.showSedanVehicleInfo();
-vehicleFiveMotorcyle.showMotorcycleVehicleInfo();
+vehicleFiveTruck.showVehicleInfo();
+vehicleFiveSedan.showVehicleInfo();
+vehicleFiveMotorcycle.showVehicleInfo();
+// vehicleFive.showVehicleInfo();
+// vehicleFiveTruck.showTruckVehicleInfo();
+// vehicleFiveSedan.showSedanVehicleInfo();
+// vehicleFiveMotorcyle.showMotorcycleVehicleInfo();
 
 
